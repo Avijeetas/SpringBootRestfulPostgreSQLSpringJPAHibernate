bae156c46a14c8a86f26b74015999a4abd2c235b
Reve System <avijeetshil110@gmail.com>
2022-07-11 12:16:01 +0600

    Initial Commit

---
+++ src/test/java/com/example/springbootrestfulpostgresqlspringjpahibernate/SpringBootRestfulPostgreSqlSpringJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootrestfulpostgresqlspringjpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulPostgreSqlSpringJpaHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
